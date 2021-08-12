a77f53af4836f5c703645a91ee600a7bd6072333
Arya Muralidharan <53123316+arya-muralidharan@users.noreply.github.com>
2021-08-12 19:23:25 -0400

    python: create c011 file

---
+++ python/s002_c011.py
@@ -0,0 +1,28 @@
+# File name: s002_c011.py
+# Description: Cryptopals Set 2, Challenge 11 - An ECB/CBC detection oracle
+# Author: Arya Muralidharan
+# Date: 2021
+
+#---MODULES---#
+
+
+#---CONSTANTS---#
+
+
+#---FUNCTIONS---#
+
+
+def main():
+    '''
+    Runs and tests the program.
+
+    Inputs: none
+
+    Returns: none
+    '''
+    return 0
+    
+
+#---RUN---#
+if __name__ == '__main__':
+    main()
